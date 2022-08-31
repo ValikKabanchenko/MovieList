a725b873868fe98c5f90adbd45ad16578f9bb5aa
ValikKabanchenko <kabanchenkoval@gmail.com>
2022-09-01 00:01:53 +0300

    Func

--- ViewController.swift
+++ ViewController.swift
@@ -12,24 +12,38 @@ class ViewController: UIViewController , UITableViewDataSource , UITableViewDele
     @IBOutlet weak var titleNameMovie: UITextField!
     
     @IBOutlet weak var titleYearMovie: UITextField!
-    
+   
+    var labelName  = []
     @IBAction func buttonAddMovie(_ sender: Any) {
         
+        if titleNameMovie.text! > " " || titleYearMovie.text! > " "{
+            labelName.append("\(titleNameMovie.text! + " " + titleYearMovie.text!)")
+        }else  {
+            print("you dont write element")
+        }
+        print(labelName)
+        
+        
     }
-    
+  
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return
+        return labelName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IndexPathone", for: indexPath)
+        
+         return cell
     }
     
 
     @IBOutlet weak var titleFilmView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        titleNameMovie.alpha = 0.5
+        titleYearMovie.alpha = 0.5
+  
     }
 
 
