cb77bd38b3b1a97fc734d6e58624ee5f92483a28
ValikKabanchenko <kabanchenkoval@gmail.com>
2022-08-31 18:45:43 +0300

    add Ui logic

--- ViewController.swift
+++ ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource , UITableViewDelegate{
+    
+    @IBOutlet weak var titleNameMovie: UITextField!
+    
+    @IBOutlet weak var titleYearMovie: UITextField!
+    
+    @IBAction func buttonAddMovie(_ sender: Any) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return
+    }
+    
 
+    @IBOutlet weak var titleFilmView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
